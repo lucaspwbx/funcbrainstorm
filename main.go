a3fc10a190499be684892a86c2f8c05f2300c448
Lucas Weiblen <lucasweiblen@gmail.com>
2014-09-20 18:10:16 -0300

    refactor former func to log and create new function to create request

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 var (
-	requestFunc = func(method, endpoint string, params Params) string {
+	logFunc = func(method, endpoint string, params Params) string {
 		debug := fmt.Sprintf("Doing %s request to: %s\n", method, endpoint)
 		debug += fmt.Sprintf("Params:")
 		debug += "--------\n"
@@ -12,33 +15,50 @@ var (
 		}
 		return debug
 	}
+
+	newRequestFunc = func(method, endpoint string, params Params) (*http.Request, error) {
+		log := logFunc(method, endpoint, params)
+		fmt.Println(log)
+		req, err := http.NewRequest(method, endpoint, nil)
+		req.SetBasicAuth("user", "password")
+		if err != nil {
+			return nil, err
+		}
+		return req, nil
+	}
 )
 
 type Params map[string]string
 
+func NewRequest() (*http.Request, error) {
+	return newRequestFunc("GET", "/contacts", nil)
+}
+
 func GetContacts(params Params) string {
-	return requestFunc("GET", "/contacts", params)
+	return logFunc("GET", "/contacts", params)
 }
 
 func CreateContact(params Params) string {
-	return requestFunc("POST", "/contacts", params)
+	return logFunc("POST", "/contacts", params)
 }
 
 func UpdateContact(params Params) string {
 	contact_iden := params["iden"]
 	endpoint := fmt.Sprintf("/contacts/%s", contact_iden)
-	return requestFunc("POST", endpoint, params)
+	return logFunc("POST", endpoint, params)
 }
 
 func DeleteContact(params Params) string {
 	contact_iden := params["iden"]
 	endpoint := fmt.Sprintf("/contacts/%s", contact_iden)
-	return requestFunc("DELETE", endpoint, params)
+	return logFunc("DELETE", endpoint, params)
 }
 
 func main() {
-	fmt.Println(GetContacts(Params{"title": "NotaUm"}))
-	fmt.Println(CreateContact(Params{"name": "Lucas", "email": "foo@bar.com"}))
-	fmt.Println(UpdateContact(Params{"iden": "1234", "name": "Lucas"}))
-	fmt.Println(DeleteContact(Params{"iden": "1234"}))
+	//fmt.Println(GetContacts(Params{"title": "NotaUm"}))
+	//fmt.Println(CreateContact(Params{"name": "Lucas", "email": "foo@bar.com"}))
+	//fmt.Println(UpdateContact(Params{"iden": "1234", "name": "Lucas"}))
+	//fmt.Println(DeleteContact(Params{"iden": "1234"}))
+	req, _ := NewRequest()
+	fmt.Println(req)
 }
