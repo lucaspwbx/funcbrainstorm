964bc3d1754b920d266184d0519e4865fc898060
Lucas Weiblen <lucasweiblen@gmail.com>
2014-09-20 19:23:36 -0300

    Add PushRequest

--- main.go
+++ main.go
@@ -6,20 +6,20 @@ import (
 )
 
 var (
-	logFunc = func(method, endpoint string, params Params) string {
-		debug := fmt.Sprintf("Doing %s request to: %s\n", method, endpoint)
+	logFunc = func(r *PushRequest) string {
+		debug := fmt.Sprintf("Doing %s request to: %s\n", r.Method, r.Endpoint)
 		debug += fmt.Sprintf("Params:")
 		debug += "--------\n"
-		for k, v := range params {
+		for k, v := range r.Params {
 			debug += fmt.Sprintf("Key: %s, Value: %s\n", k, v)
 		}
 		return debug
 	}
 
-	newRequestFunc = func(method, endpoint string, params Params) (*http.Request, error) {
-		log := logFunc(method, endpoint, params)
+	newRequestFunc = func(r *PushRequest) (*http.Request, error) {
+		log := logFunc(r)
 		fmt.Println(log)
-		req, err := http.NewRequest(method, endpoint, nil)
+		req, err := http.NewRequest(r.Method, r.Endpoint, nil)
 		req.SetBasicAuth("user", "password")
 		if err != nil {
 			return nil, err
@@ -28,37 +28,47 @@ var (
 	}
 )
 
+type PushRequest struct {
+	Method   string
+	Endpoint string
+	Params   Params
+}
+
 type Params map[string]string
 
-func NewRequest() (*http.Request, error) {
-	return newRequestFunc("GET", "/contacts", nil)
+func NewPushRequest(method, endpoint string, params Params) *PushRequest {
+	return &PushRequest{Method: method, Endpoint: endpoint, Params: params}
+}
+
+func NewHttpRequest() (*http.Request, error) {
+	return newRequestFunc(NewPushRequest("GET", "/contacts", nil))
 }
 
 func GetContacts(params Params) string {
-	return logFunc("GET", "/contacts", params)
+	//return logFunc("GET", "/contacts", params)
+	return "not implemented"
 }
 
 func CreateContact(params Params) string {
-	return logFunc("POST", "/contacts", params)
+	//return logFunc("POST", "/contacts", params)
+	return "not implemented"
 }
 
 func UpdateContact(params Params) string {
-	contact_iden := params["iden"]
-	endpoint := fmt.Sprintf("/contacts/%s", contact_iden)
-	return logFunc("POST", endpoint, params)
+	//contact_iden := params["iden"]
+	//endpoint := fmt.Sprintf("/contacts/%s", contact_iden)
+	//return logFunc("POST", endpoint, params)
+	return "not implemented"
 }
 
 func DeleteContact(params Params) string {
-	contact_iden := params["iden"]
-	endpoint := fmt.Sprintf("/contacts/%s", contact_iden)
-	return logFunc("DELETE", endpoint, params)
+	//contact_iden := params["iden"]
+	//endpoint := fmt.Sprintf("/contacts/%s", contact_iden)
+	//return logFunc("DELETE", endpoint, params)
+	return "not implemented"
 }
 
 func main() {
-	//fmt.Println(GetContacts(Params{"title": "NotaUm"}))
-	//fmt.Println(CreateContact(Params{"name": "Lucas", "email": "foo@bar.com"}))
-	//fmt.Println(UpdateContact(Params{"iden": "1234", "name": "Lucas"}))
-	//fmt.Println(DeleteContact(Params{"iden": "1234"}))
-	req, _ := NewRequest()
+	req, _ := NewHttpRequest()
 	fmt.Println(req)
 }
