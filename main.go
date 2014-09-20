6cf7f80cad230727996120ee1346efd89615feb1
Lucas Weiblen <lucasweiblen@gmail.com>
2014-09-20 17:26:29 -0300

    :scissors:

--- main.go
+++ main.go
@@ -3,60 +3,42 @@ package main
 import "fmt"
 
 var (
-	funcao = func(method, endpoint string, params Params) string {
-		teste := fmt.Sprintf("Doing %s request to: %s", method, endpoint)
-		teste += "\n"
-		teste += fmt.Sprintf("Params:")
-		teste += "--------\n"
+	requestFunc = func(method, endpoint string, params Params) string {
+		debug := fmt.Sprintf("Doing %s request to: %s\n", method, endpoint)
+		debug += fmt.Sprintf("Params:")
+		debug += "--------\n"
 		for k, v := range params {
-			teste += fmt.Sprintf("Key: %s, Value: %s", k, v)
-			teste += "\n"
+			debug += fmt.Sprintf("Key: %s, Value: %s\n", k, v)
 		}
-		return teste
+		return debug
 	}
 )
 
 type Params map[string]string
 
 func GetContacts(params Params) string {
-	//fn := func(method, endpoint string, params Params) string {
-	//teste := fmt.Sprintf("Doing %s request to: %s", method, endpoint)
-	//teste += "\n"
-	//teste += fmt.Sprintf("Params:")
-	//teste += "--------\n"
-	//for k, v := range params {
-	//teste += fmt.Sprintf("Key: %s, Value: %s", k, v)
-	//}
-	//return teste
-	//}
-	//return fn("GET", "/contacts", params)
-	return funcao("GET", "/contacts", params)
+	return requestFunc("GET", "/contacts", params)
 }
 
 func CreateContact(params Params) string {
-	return funcao("POST", "/contacts", params)
+	return requestFunc("POST", "/contacts", params)
 }
 
 func UpdateContact(params Params) string {
-	//fn := func(method, endpoint string, params Params) string {
-	//teste := fmt.Sprintf("Doing %s request to: %s", method, endpoint)
-	//teste += "\n"
-	//teste += fmt.Sprintf("Params:")
-	//teste += "--------\n"
-	//for k, v := range params {
-	//teste += fmt.Sprintf("Key: %s, Value: %s", k, v)
-	//teste += "\n"
-	//}
-	//	return teste
-	//}
-	//return fn("POST", "/contacts", params)
-	iden := params["iden"]
-	endpoint := fmt.Sprintf("/contacts/%s", iden)
-	return funcao("POST", endpoint, params)
+	contact_iden := params["iden"]
+	endpoint := fmt.Sprintf("/contacts/%s", contact_iden)
+	return requestFunc("POST", endpoint, params)
+}
+
+func DeleteContact(params Params) string {
+	contact_iden := params["iden"]
+	endpoint := fmt.Sprintf("/contacts/%s", contact_iden)
+	return requestFunc("DELETE", endpoint, params)
 }
 
 func main() {
 	fmt.Println(GetContacts(map[string]string{"title": "NotaUm"}))
 	fmt.Println(CreateContact(map[string]string{"name": "Lucas", "email": "foo@bar.com"}))
 	fmt.Println(UpdateContact(map[string]string{"iden": "1234", "name": "Lucas"}))
+	fmt.Println(DeleteContact(map[string]string{"iden": "1234"}))
 }
