7ff125ef3dfb0c4d523e8358ad6231760e618e2a
Lucas Weiblen <lucasweiblen@gmail.com>
2014-09-20 23:02:41 -0300

    add do request and parse response functions

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -27,6 +28,14 @@ var (
 		}
 		return req, nil
 	}
+
+	getResponseFunc = func(r *http.Request) (PushResponse, error) {
+		response, err := makeRequest(r)
+		if err != nil {
+			return nil, err
+		}
+		return response, nil
+	}
 )
 
 type PushRequest struct {
@@ -37,12 +46,40 @@ type PushRequest struct {
 
 type Params map[string]string
 
+type PushResponse map[string]interface{}
+
+func makeRequest(r *http.Request) (PushResponse, error) {
+	httpClient := &http.Client{}
+	resp, err := httpClient.Do(r)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var response PushResponse
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
 func NewPushRequest(method, endpoint string, params Params) *PushRequest {
 	return &PushRequest{Method: method, Endpoint: endpoint, Params: params}
 }
 
+func ParseResponse(req *http.Request) (map[string]interface{}, error) {
+	resp, err := getResponseFunc(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
 func GetContacts() (*http.Request, error) {
-	return newRequestFunc(NewPushRequest("GET", "/contacts", nil))
+	//return newRequestFunc(NewPushRequest("GET", "/contacts", nil))
+	//return newRequestFunc(NewPushRequest("GET", "http://www.google.com", nil))
+	return newRequestFunc(NewPushRequest("GET", "https://api.pushbullet.com/v2/contacts", nil))
 }
 
 func CreateContact(params Params) (*http.Request, error) {
@@ -68,7 +105,7 @@ func DeleteContact(params Params) (*http.Request, error) {
 	return newRequestFunc(request)
 }
 
-func ParseParamsId(params Params) (endpoint string, err error) {
+func ParseParamsId(params Params) (string, error) {
 	if id, ok := params["iden"]; ok {
 		return fmt.Sprintf("/contacts/%s", id), nil
 	}
@@ -84,4 +121,11 @@ func main() {
 	fmt.Println(req3)
 	req4, _ := DeleteContact(Params{"iden": "123"})
 	fmt.Println(req4)
+	fmt.Println(req)
+	bla, err := ParseResponse(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(bla)
 }
